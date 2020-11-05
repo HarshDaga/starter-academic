dcaf4ae757e29d50bf7cf103c71d73c1a0f6489d
Harsh Daga <harsh.daga.tech@gmail.com>
2020-11-05 05:08:09 -0600

    Update Data Mining Assignment 02

--- static/coursework/data mining/assignments/02/Data.py
+++ static/coursework/data mining/assignments/02/Data.py
@@ -1,10 +1,40 @@
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import KFold, train_test_split
 
 
 class Data:
-    def __init__(self, filename, split_frac):
-        self.features = ['sepal length', 'sepal width', 'petal length', 'petal width']
-        self.columns = self.features + ['class']
-        self._df = pd.read_csv(filename, header=None, names=self.columns)
-        self.dev = self._df.sample(frac=split_frac)
-        self.test = self._df.drop(self.dev.index)
+    features: list = ['sepal length', 'sepal width', 'petal length', 'petal width']
+    columns: list = features + ['class']
+
+    def __init__(self, df, train, dev, test):
+        self._df: pd.DataFrame = df.copy()
+        self.train: pd.DataFrame = train.copy()
+        self.dev: pd.DataFrame = dev.copy()
+        self.test: pd.DataFrame = test.copy()
+        self.min = self.test[self.features].min()
+        self.max = self.test[self.features].max()
+
+    @classmethod
+    def read(cls, filename: str, n_splits = 5, train_dev_split = 0.75):
+        df = pd.read_csv(filename, header=None, names=cls.columns)
+        kf = KFold(n_splits=n_splits, shuffle=True)
+        for train_index, test_index in kf.split(df):
+            train = df.loc[train_index]
+            test = df.loc[test_index]
+            train, dev = train_test_split(train, train_size=train_dev_split)
+
+            yield cls(df, train, dev, test)
+
+    def normalized(self):
+        result = self.copy()
+        for df in (result._df, result.train, result.dev, result.test):
+            df[result.features] = (df[result.features] - result.min) / (result.max - result.min)
+
+        return result
+
+    def normalize(self, points: np.ndarray):
+        return (points - self.min.to_numpy()) / (self.max.to_numpy() - self.min.to_numpy())
+
+    def copy(self):
+        return Data(self._df, self.train, self.dev, self.test)
--- static/coursework/data mining/assignments/02/kNN.py
+++ static/coursework/data mining/assignments/02/kNN.py
@@ -1,59 +1,47 @@
-from typing import Callable, List
+from typing import Callable
 
 import numpy as np
 import pandas as pd
+from Data import Data
 
 
 class kNN:
     def __init__(self,
-                 dev_data: pd.DataFrame,
-                 features: List[str],
-                 distance_func: Callable[[np.ndarray, np.ndarray], float],
-                 use_normalization=False):
-        self.dev = dev_data.copy()
-        self.features = features
+                 data: Data,
+                 distance_func: Callable[[np.ndarray, np.ndarray], float]):
+        self.data: Data = data.copy()
         self.distance_func = distance_func
-        self.use_normalization = use_normalization
-        self.min, self.max = 0, 1  # (0, 1) defaults behave the same as no normalization
-
-        if self.use_normalization:
-            df = self.dev[self.features]
-            self.min = df.min().to_numpy()
-            self.max = df.max().to_numpy()
-            self.dev[self.features] = (df - df.min()) / (df.max() - df.min())
-
-    def normalize(self, point: np.ndarray):
-        return (point - self.min) / (self.max - self.min)
+        self.prediction_data = pd.concat([self.data.train, self.data.dev])
 
     def predict(self, point: np.ndarray, k):
-        return self._predict_internal(self.dev, self.normalize(point), k)
+        return self._predict_internal(self.prediction_data, point, k)
 
-    def calc_accuracy(self, test_data: pd.DataFrame, k):
-        points = test_data[self.features].to_numpy()
+    def final_accuracy(self, k):
+        points = self.data.test[self.data.features].to_numpy()
         success = 0
         for i, point in enumerate(points):
             pred = self.predict(point, k)
-            real = test_data['class'].iloc[i]
+            real = self.data.test['class'].iloc[i]
             if pred == real:
                 success += 1
 
         return success / points.shape[0]
 
     def k_accuracy(self, k):
-        points = self.dev[self.features].to_numpy()
+        points = self.data.dev[self.data.features].to_numpy()
         success = 0
         for i, point in enumerate(points):
-            pred = self._predict_internal(self.dev.drop(self.dev.index[i]), point, k)
-            real = self.dev['class'].iloc[i]
+            pred = self._predict_internal(self.data.train, point, k)
+            real = self.data.dev['class'].iloc[i]
             if pred == real:
                 success += 1
 
         return success / points.shape[0]
 
     def _predict_internal(self, data, point: np.ndarray, k):
-        distances = data[self.features].to_numpy(copy=True)
+        distances = data[self.data.features].to_numpy()
         dist_transform = lambda x: self.distance_func(x, point)
         distances = np.apply_along_axis(dist_transform, 1, distances)
 
-        neighbors = data.assign(d=distances).sort_values('d')[:k]
+        neighbors = data.assign(d=distances).nsmallest(k, 'd')
         return neighbors['class'].mode()[0]
